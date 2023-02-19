f65be70f32eae3758ea116b445b38e1ddb272e09
Alex Bezuska <abezuska@switcherstudio.com>
2023-02-18 23:35:34 -0500

    update to use static site generator

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
 New website for 2023!
 
 
+This is a static site build with a simple generator.
+
+## Prereqs
+
+1. Install node 
+2. Navigate to the root of this project in your terminal
+3. run `npm i` to install the node modules used by the project
+
+
+## To build:
+
+1. Navigate to the root of this project in your terminal
+2. Run `npm start` from the terminal, then files will magically appear in the `dest` folder.
+
+
+### To preview it
+
+1. Run `npm run serve` and it will do a python thing and let you view the site at port 8000
+
+
+### Now you need to get those files onto a server.
+
+1. 'npm run upload'
---
+++ static-site-generator.js
@@ -0,0 +1,82 @@
+var fs = require("fs");
+var path = require("path");
+
+var handlebars = require("handlebars");
+var copydir = require("copy-dir");
+var config = require("./config.json");
+var site = require("./site.json");
+var src = "./" + config.src;
+var dest = "./" + config.dest;
+
+makeDirIfNotExist(dest);
+makeDirIfNotExist(path.join(dest, "css"));
+copydir.sync(path.join(src, "css"), path.join(dest, "css"));
+makeDirIfNotExist(path.join(dest, "img"));
+copydir.sync(path.join(src, "img"), path.join(dest, "img"));
+makeDirIfNotExist(path.join(dest, "js"));
+copydir.sync(path.join(src, "js"), path.join(dest, "js"));
+
+let data = {};
+data.site = site;
+addPageData(data);
+
+function addPageData(data) {
+  var newData = data;
+  newData.site.pages.forEach((page) => {
+    if (page.showInNav){
+      var pageName = page.name.toLowerCase();
+      var jsonPath = path.join(src, "pages", pageName + ".json");
+      if (fs.existsSync(jsonPath)){
+        newData[pageName] = require("./" + jsonPath);
+      }
+    }
+  });
+  return newData;
+}
+
+fs.writeFileSync("./data.json", JSON.stringify(data, null, 2));
+
+data.site.pages.forEach((page) => {
+  var pageName = page.name.toLowerCase();
+  var fileName = page.file;
+  createPage(pageName, data, path.join(dest, fileName));
+});
+
+function insertPartials(templateName) {
+  var completeTemplate = fs.readFileSync(path.join(src, config.templatesDirectory, templateName + ".hbs"), "utf8");
+  config.partials.forEach((partial) => {
+    var partialTemplate = fs.readFileSync(path.join(src, config.partialsDirectory, partial + ".hbs"), "utf8");
+    var partialTag = "{{>tag}}".replace("tag", partial);
+    completeTemplate = completeTemplate.replace(partialTag, partialTemplate);
+  });
+  return completeTemplate;
+}
+
+function createPage(templateName, data, outputFileName) {
+  var html = renderFromExternalTemplate(insertPartials(templateName), data);
+  fs.writeFileSync(outputFileName, html);
+}
+
+function renderFromExternalTemplate(template, data){
+  var template = handlebars.compile(template);
+  return template(data);
+}
+
+function truncate(string, length){
+  if (string.length > length)
+  return string.substring(0, length)+'...';
+  else
+  return string;
+};
+
+function getDirectories(path) {
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path+'/'+file).isDirectory();
+  });
+}
+
+function makeDirIfNotExist(filePath) {
+  if (!fs.existsSync(filePath)){
+    fs.mkdirSync(filePath);
+  }
+}
